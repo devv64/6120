4c575f68b8da3a5e2c54f6519df6570232a6a260
devv64 <devextraemail@gmail.com>
2025-02-06 21:03:52 -0500

    lvn working except weird case

--- lesson3/lvn.py
+++ lesson3/lvn.py
@@ -17,10 +17,10 @@ Essentially: # Variables Defined <= # Values Used
 
 # ! should do some preprocessing pass such that I rename the earlier instance of a variable if its reused
 def lvn(blocks):
-    CurVarToId = {} # var to id map
-    VarToVal = [] # this is the table represented as list of tuple (var, val), list index is ID
     # need to define val - (op, args...)
     for block in blocks:
+        CurVarToId = {} # var to id map
+        VarToVal = [] # this is the table represented as list of tuple (var, val), list index is ID
         for instr in block:
             # * if value in table
             if 'dest' in instr:
@@ -30,13 +30,28 @@ def lvn(blocks):
                     # this .get part might need refactoring... this is essentially the case where variable is not defined in the scope of \
                     # the current block, we might want to keep track of this in the table
                     # ! if VarToVal of this returns a constant, use the actual value instead of variable ([1] instead of [0])
-                    value = (instr['op'], [VarToVal[CurVarToId.get(arg, arg)][0] for arg in instr.get('args', [])])
+                    args_list = []
+                    for arg in instr.get('args', []):
+                        # * Undefined variable in scope
+                        if CurVarToId.get(arg, None) != None:
+                            args_list.append(VarToVal[CurVarToId.get(arg, arg)][0])
+                        else:
+                             VarToVal.append((arg, arg))
+                             args_list.append([arg])
+                             CurVarToId[arg] = len(VarToVal)-1
+                            
+                    value = (instr['op'], args_list)
 
                 # * find index and var if value is in VarToVal already
-                num, var = next(((i, k ) for i, (k, v) in enumerate(VarToVal) if v == value), (None, None))
+                if not isinstance(value, bool):
+                    num, var = next(((i, k) for i, (k, v) in enumerate(VarToVal) if v == value and not isinstance(v, bool)), (None, None))
+
 
                 # * if value in table
-                if var:
+                # ! this is not working completely right
+                # ! bc of how i insert id commands into here, i should be checking differently
+                # ! instead of replacing when duplicate values are id'd, replaces duplicate actual id function usages
+                if var and instr['op'] != 'id':
                     # replace instr with copy of var
                     # ! here is where i can do const prop, keep track of constants, check if constant, replace w constant
                     instr.update({'op': 'id', 'args': [var]})
@@ -62,7 +77,7 @@ if __name__ == "__main__":
 	for func in prog['functions']:
 		blocks = create_blocks(func['instrs'])
 		lvn(blocks)
-		func['instrs'] = [instr for block in blocks for instr in block if instr]  
+		func['instrs'] = [instr for block in blocks for instr in block if instr]
 	
 	json.dump(prog, sys.stdout, indent=2, sort_keys=True)
 	
